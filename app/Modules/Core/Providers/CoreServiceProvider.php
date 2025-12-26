b52712a614f6aef2071f0001f4de8a099d2454e4
Alex Caron <alex@alexcaron.ca>
2025-12-26 07:03:47 -0500

    fix: register Livewire components in CoreServiceProvider

--- app/Modules/Core/Providers/CoreServiceProvider.php
+++ app/Modules/Core/Providers/CoreServiceProvider.php
@@ -2,9 +2,17 @@
 
 namespace App\Modules\Core\Providers;
 
+use App\Modules\Core\Livewire\Dashboard;
+use App\Modules\Core\Livewire\DataExport;
+use App\Modules\Core\Livewire\DataImport;
+use App\Modules\Core\Livewire\RoleForm;
+use App\Modules\Core\Livewire\RoleList;
+use App\Modules\Core\Livewire\UserForm;
+use App\Modules\Core\Livewire\UserList;
 use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\ServiceProvider;
+use Livewire\Livewire;
 
 class CoreServiceProvider extends ServiceProvider
 {
@@ -22,5 +30,14 @@ class CoreServiceProvider extends ServiceProvider
 
         // Register Blade components
         Blade::componentNamespace('App\\Modules\\Core\\Views\\Components', 'core');
+
+        // Register Livewire components
+        Livewire::component('dashboard', Dashboard::class);
+        Livewire::component('data-export', DataExport::class);
+        Livewire::component('data-import', DataImport::class);
+        Livewire::component('role-form', RoleForm::class);
+        Livewire::component('role-list', RoleList::class);
+        Livewire::component('user-form', UserForm::class);
+        Livewire::component('user-list', UserList::class);
     }
 }
